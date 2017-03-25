6872cdde2a61ebae387c1cbc6156286bec326c4f
hunar-1997 <hbkurd@gmail.com>
2017-03-25 20:25:18 +0300

    Created the board with the pieces in it

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Chess
+
+The first time i try to make a chess engine
+
+Features to implement:
+selectable players (ai vs ai  human vs human  human vs ai  ai vs human)
--- main.cpp
+++ main.cpp
@@ -1,45 +1,103 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <random>
+#include <vector>
 
 #define WIDTH 600
 #define HEIGHT 600
 #define SIZE WIDTH/8
 
-class vec2{
+// These are Kurdish names
+
+enum Type_of_pice {
+	pasha, wazir, fil, asp, qala, sarbaz, batal
+}; // In English {king, queen, bishop, knight, rook, pawn, empty}
+
+enum Color_of_pice {
+	spi, rash
+}; // In English {white, black}
+
+class vec2 {
 public:
 	int x, y;
-	vec2(){};
-	vec2(int x, int y){
+
+	vec2() {
+	};
+
+	vec2(int x, int y) {
 		this->x = x;
 		this->y = y;
 	};
 };
 
+class piece {
+public:
+	int type;
+	int color;
+
+	piece() {
+	};
+
+	piece(int type, int color) {
+		this->type = type;
+		this->color = color;
+	}
+};
+
+sf::Texture pieces[12];
+piece board[8][8];
+
+sf::Texture *get_texture(int type, int color) {
+	return &pieces[type + color * 6];
+}
+
 int main(int argc, char** argv) {
+	// Display stuff
 	sf::RenderWindow screen(sf::VideoMode(WIDTH, HEIGHT), "Chess - made by hunar - hbkurd.weebly.com");
 	screen.setFramerateLimit(24);
-	
-	sf::Image dashs_image;
-	dashs_image.loadFromFile("sprites.png");
-	
-	sf::Texture dashes[12];
+	// ----------------------------
+
+	// Textures of the pices
+	sf::Image pieces_image;
+	pieces_image.loadFromFile("sprites.png");
+
 	for (int y = 0; y < 2; y++)
 		for (int x = 0; x < 6; x++)
-			dashes[6 * y + x].loadFromImage(dashs_image, sf::IntRect(x * SIZE, y * SIZE, SIZE, SIZE));
-	
-	
+			pieces[6 * y + x].loadFromImage(pieces_image, sf::IntRect(x * SIZE, y * SIZE, SIZE, SIZE));
+	// ----------------------------
+
+	// Creating the starting board
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			int color;
+			if (i > 3) color = spi;
+			else color = rash;
+
+			if (i == 1) board[i][j] = piece(sarbaz, color);
+			else if (i == 6) board[i][j] = piece(sarbaz, color);
+			else if ( i==7 || i==0 ){
+				int tp[] = {qala, asp, fil, wazir, pasha, fil, asp, qala};
+				board[i][j] = piece(tp[j], color);
+			}
+			
+
+			else board[i][j] = piece(batal, rash);
+		}
+	}
+	// ----------------------------
+
+
 	// background
 	sf::Texture back_tx;
 	back_tx.loadFromFile("board.png");
 	back_tx.setRepeated(true);
 	sf::Sprite background;
 	background.setTexture(back_tx);
-	background.setTextureRect({ 0, 0, WIDTH, HEIGHT });
-	
+	background.setTextureRect({0, 0, WIDTH, HEIGHT});
+	// ----------------------------
+
 	bool should_redraw = true;
 	while (screen.isOpen()) {
-		
+
 		sf::Event events;
 		while (screen.pollEvent(events)) {
 			if (events.type == sf::Event::Closed) {
@@ -51,14 +109,24 @@ int main(int argc, char** argv) {
 				}
 			}
 		}
-		
-		if (should_redraw){
+
+		if (should_redraw) {
 			screen.draw(background);
-			
-			sf::Sprite jun;
-			jun.setTexture(dashes[5]);
-			screen.draw(jun);
-			
+
+			// Drawing the board
+			sf::Sprite s;
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					piece _this = board[i][j];
+					if (_this.type != batal) {
+						s.setTexture(*get_texture(_this.type, _this.color));
+						s.setPosition(j*SIZE, i * SIZE);
+						screen.draw(s);
+					}
+				}
+			}
+			// ---------------------------
+
 			should_redraw = false;
 		}
 
